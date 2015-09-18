07b434e5110274a3cc178e6381bafb9138206262
Busyra Anwar <busyra@me.com>
2015-09-18 00:27:04 -0700

    tested angularized list

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,6 +1,13 @@
 'use strict';
 
-angular.module('myApp')
-    .controller('MainCtrl', function($scope){
-        $scope.todos=['Item 1','Item 2','Item 3'];
-    });
+/**		+angular.module('myApp')
+- * @ngdoc function		+    .controller('MainCtrl', function($scope){
+- * @name yeomanApp.controller:MainCtrl		+        $scope.todos=['Item 1','Item 2','Item 3'];
+- * @description		+    });
+- * # MainCtrl
+- * Controller of the yeomanApp
+- */
+angular.module('yeomanApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.todos = ['Item 1', 'Item 2', 'Item 3', 'Item 4'];
+  });
